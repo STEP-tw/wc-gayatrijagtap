15e92c32243af97eb5b93242cb0bf0a6c1539def
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-12-26 14:45:20 +0530

    implemented counter and formatOutput
    moved line,byte and word counting into counter
    moved formating of the final output to formatOutput

--- src/lib.js
+++ src/lib.js
@@ -8,13 +8,24 @@ const splitByWhiteSpace = (text) => text.split(NEW_LINE).join(SPACE).split(SPACE
 
 const countWords = (words) => words.filter(word => word != false).length;
 
-const wc = function (file, fs) {
-    let content = fs.readFileSync(file, 'utf8');
+const counter = function (content) {
     let lineCount = countNewLines(content);
     let byteCount = countBytes(content);
     let words = splitByWhiteSpace(content);
     let wordCount = countWords(words);
-    let output = ['', lineCount, wordCount, byteCount].join(TAB) + SPACE + file;
-    return output;
+    return [lineCount, wordCount, byteCount];
+}
+
+const formatOutput = function (countArray, file) {
+    countArray.unshift(EMPTY_STRING);
+    let formatedOutput = countArray.join(TAB) + SPACE + file;
+    return formatedOutput;
+}
+
+const wc = function (file, fs) {
+    let content = fs.readFileSync(file, 'utf8');
+    let countArray = counter(content);
+    let formatedOutput = formatOutput(countArray, file);
+    return formatedOutput;
 }
 module.exports = { wc };
